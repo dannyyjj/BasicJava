1ccf133b5c3e3ba6594161aeaa356165e0790c67
Danny <dannyyjj123@gmail.com>
2023-02-06 22:59:21 +0900

    상속 예제 주석추가

--- src/Object_oriented_Programing/inheritance/CaptionTvTest.java
+++ src/Object_oriented_Programing/inheritance/CaptionTvTest.java
@@ -28,3 +28,10 @@ public class CaptionTvTest {
         ctv.displayCaption("Hello, world");
     }
 }
+
+// TV클래스로부터 상속받고 기능을 추가하여 CaptionTv클래스 생성
+// 멤버변수 caption은 캡션기능의 상태를 저장하기 위한 boolean형 변수
+// displayCaption(String text)은 매개변수로 넘겨받은 문자열(text)을 캡션이 켜져있는 경우(caption의 값이 true인 경우)에만 화면에 출력
+
+// 자손 클래스의 인스턴스를 생성하면 조상 클래스의 멤버도 함께 생성되기 때문에
+// 따로 조상클래스의 인스턴스를 생성하지 않고도 조상 클래스들의 멤버들을 사용할 수 있음

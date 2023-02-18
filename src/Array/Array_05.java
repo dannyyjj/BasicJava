7d698eaf42bbb7b1b02b50029381bdfee2d8c130
Danny <dannyyjj123@gmail.com>
2023-02-18 23:06:40 +0900

    섞기 예제

---
+++ src/Array/Array_05.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class Array_05 {
+    public static void main(String[] args) {
+        int[] ball = new int[45];
+
+        for (int i = 0; i < ball.length; i++) {
+            ball[i] = i + 1;
+        }
+
+        int temp = 0;
+        int j = 0;
+        for (int i = 0; i < 6; i++) {
+            j = (int) (Math.random() * 45);
+            temp = ball[i];
+            ball[i] = ball[j];
+            ball[i] = temp;
+        }
+
+        for (int i = 0; i<6; i++ ) {
+            System.out.println("");
+        }
+    }
+}
